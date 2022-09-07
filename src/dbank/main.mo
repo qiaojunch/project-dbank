0bbe7fa28f47746eaca1738cfd68dde9cbdce715
qiaojunch <qiaojunch@gmail.com>
2022-09-07 14:12:51 -0700

    finish input and withdraw tokens

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,54 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+// create a class/actor to hold Cannister
+actor DBank {
+  stable var currentValue: Float = 300;
+  // currentValue := 100; 
+
+  stable var startTime = Time.now();
+  // startTime := Time.now();
+  Debug.print(debug_show(startTime));
+
+  // Debug.print(debug_show(currentValue));
+
+  // increase the current value in dbank
+  public func topUp(amount: Float) {
+    currentValue += amount;
+    Debug.print(debug_show(currentValue));
   };
-};
+
+  // decrease the current value in dbank
+  public func withdraw(amount: Float) {
+    let tmpValue: Float = currentValue - amount;
+    if (tmpValue >= 0) {
+      currentValue -= amount;
+      Debug.print(debug_show(currentValue));
+    } else {
+      Debug.print("Amount too large, current value less than zero");
+    }
+  };
+
+  // query call: do not modify blockchain; faster to compiler
+  public query func checkBalance(): async Float {
+    return currentValue;
+  };
+
+
+  // calc compound interest with interest rate of 0.01 and time of second 
+  // which means that interest is collected per second 
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000;
+
+    // update current balance after compounding 
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS));
+
+    // reset the start time to current to avoid recalucalte the compound interet 
+    startTime := currentTime;
+  };
+
+
+}
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,46 @@
+import { UpdateCallRejectedError } from "@dfinity/agent";
 import { dbank } from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+// update current balance in bank when page is loaded
+window.addEventListener("load", async function() {
+    // console.log("page loaded");
+    update();
 });
+
+// increase balance by input amount
+document.querySelector("form").addEventListener("submit", async function(event) {
+    event.preventDefault();    // prevent auto-fresh due to property of form
+
+    const button = event.target.querySelector("#submit-btn");
+    const inputAmount = parseFloat(document.getElementById("input-amount").value);
+    const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+    // disable the submit button while waiting to increase balance
+    button.setAttribute("disabled", true);
+
+    if (document.getElementById("input-amount").value.length != 0) {
+        await dbank.topUp(inputAmount);
+    }
+
+    if (document.getElementById("withdrawal-amount").value.length != 0) {
+        await dbank.withdraw(outputAmount);
+    }
+
+    await dbank.compound();
+
+    // show update balance on window
+    update();
+   
+    // clear input/withdraw area afterward
+    document.getElementById("input-amount").value = "";
+    document.getElementById("withdrawal-amount").value = "";
+
+    // reset the submit button to work
+    button.removeAttribute("disabled");
+});
+
+// update the current balance in bank 
+async function update() {
+    const currentValue = await dbank.checkBalance();
+    document.getElementById("value").innerText = Math.round(currentValue * 100) / 100;
+}
